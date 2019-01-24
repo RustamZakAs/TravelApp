f7f19324a3fa3b5640b8c799032128ad2b4964de
RustamZakAs <rostik055@gmail.com>
2019-01-24 23:58:20 +0400

    CheckConnection

--- Models/Ip.cs
+++ Models/Ip.cs
@@ -146,6 +146,26 @@ namespace TravelApp.Models
             //return ipInfo.Country;
             return ipInfo;
         }
+
+        public static bool CheckConnection(String URL)
+        {
+            try
+            {
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(URL);
+                request.Timeout = 5000;
+                request.Credentials = CredentialCache.DefaultNetworkCredentials;
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+                if (response.StatusCode == HttpStatusCode.OK)
+                    return true;
+                else
+                    return false;
+            }
+            catch
+            {
+                return false;
+            }
+        }
     }
 
     public class IpInfo
